a222596a955cdf572216b51c4f374307d0eff4e3
Alif Imtiaj <lolalif412@gmail.com>
2025-11-24 08:45:17 +0600

    Update account.cpp

--- BankSystem/src/account.cpp
+++ BankSystem/src/account.cpp
@@ -1,8 +1,11 @@
 #include "account.h"
 #include <iostream>
 #include <limits>
+#include <fstream>
 
 void account::CreateNewAccount() {
+
+	
 	std::cout << "Enter full name: ";
 	std::getline(std::cin, m_fullname);
 	std::cout << "Enter date of birth (format: dd/mm/yyyy): ";
@@ -26,9 +29,23 @@ void account::CreateNewAccount() {
 	std::cout << "Enter E-Mail: ";
 	std::getline(std::cin, m_email);
 
-	std::cout << "Account created successfully!!\n";
+	std::string fileName = m_fullname + ".txt";
+	std::fstream stream(fileName.c_str(), std::ios::out);
+	if (stream.is_open()) {
+		stream << "Full Name: " << m_fullname << std::endl;
+		stream << "Date of birth: " << m_dob << std::endl;
+		if (m_gender == Gender::MALE)
+			stream << "Gender: Male\n";
+		else
+			stream << "Gender: Female\n";
 
-
+		stream << "Address: " << m_address << std::endl;
+		stream << "E-Mail: " << m_email << std::endl;
+		std::cout << "Account created successfully!!\n";
+	}
+	else {
+		std::cout << "File creation failed!!!\n";
+	}
 	
 }
 
